3a3a51796925eb9a7675f38cc9e8b427ea67c509
Alex Kalish <apkanalytics21@gmail.com>
2026-04-23 15:51:55 -0500

    Configure Terraform GCS backend

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "gcs" {
+    bucket = "akalish-software-tfstate-698032141114"
+    prefix = "health-fitness-coach/prod"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
