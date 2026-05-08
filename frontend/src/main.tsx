73571c10f12a307156b9092737db6f885ac66028
nate-dash <nate@thedashforward.com>
2026-05-08 10:30:16 -0400

    PWA Support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
 import { createRouter } from "./app/router";
+import { registerServiceWorker } from "./pwa";
 import "./index.css";
 
 const queryClient = new QueryClient({
@@ -16,6 +17,8 @@ const queryClient = new QueryClient({
 });
 
 const router = createRouter();
+
+registerServiceWorker();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
